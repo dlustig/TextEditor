7507b9ed39ba47270ebefc761811fdcd65dfb4e1
ensign-redshirt <alewis3@mail.umw.edu>
2013-11-06 12:15:19 -0500

    Update Export.java

--- Export.java
+++ Export.java
@@ -18,38 +18,38 @@ import java.util.*;
 
 //This class uses the SingleInstance design,as there should only be 1 search box per instance of the program.
 
-public class Export  extends JDialog{
-	
-	//Parent window objects
-	private Window owner;
-	private Document doc;
+public class Export extends JDialog{
+        
+        //Parent window objects
+        private Window owner;
+        private Document doc;
 
-	//Swing components to be displayed
-	private JTextComponent comp;
-	private JTextField searcher;
-		
-	private static BufferedReader input;
-	private static JFileChooser fc;
-	
-	
-	public Export(Window window, JTextComponent pDoc) {
-		
-		comp = pDoc;
-		doc = pDoc.getDocument();
-		String save = pDoc.getText();
-        	JFileChooser chooser = new JFileChooser();
+        //Swing components to be displayed
+        private JTextComponent comp;
+        private JTextField searcher;
+                
+        private static BufferedReader input;
+        private static JFileChooser fc;
+        
+        
+        public Export(Window window, JTextComponent pDoc) {
+                
+                comp = pDoc;
+                doc = pDoc.getDocument();
+                String save = pDoc.getText();
+                JFileChooser chooser = new JFileChooser();
        
-		 //chooser.setCurrentDirectory(new File("/home/me/Documents"));
-	        int retrival = chooser.showSaveDialog(null);
-        	if (retrival == JFileChooser.APPROVE_OPTION) {
-            		try {
-                		FileWriter fw = new FileWriter(chooser.getSelectedFile()+".txt");
-                		fw.write(save.toString());
-        		 fw.close();
-            		} catch (Exception ex) {
-        		 ex.printStackTrace();
-            		}
-		 }
-		
-	}
+                 //chooser.setCurrentDirectory(new File("/home/me/Documents"));
+         int retrival = chooser.showSaveDialog(null);
+                if (retrival == JFileChooser.APPROVE_OPTION) {
+                            try {
+                                FileWriter fw = new FileWriter(chooser.getSelectedFile()+".txt");
+                                fw.write(save.toString());
+                         fw.close();
+                            } catch (Exception ex) {
+                         ex.printStackTrace();
+                            }
+                 }
+                
+        }
 }
